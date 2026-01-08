2b83385ba3b13ea1cdb80bcff2565880415e50c2
firatenol <e.karuzin@inbox.ru>
2026-01-08 18:44:54 +0300

    Надеюсь стабильная версия первого этапа

--- flowers/src/main/java/shop_of_flowers/com/flowers/FlowersApplication.java
+++ flowers/src/main/java/shop_of_flowers/com/flowers/FlowersApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlowersApplication.class, args);
 	}
